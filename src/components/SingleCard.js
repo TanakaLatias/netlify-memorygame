d5398bfe710531ad9b0382ae46deb57646de62c0
Sawa <sawa@sawa.com>
2024-07-30 20:11:41 +0900

    fixed package*.json, src/components/SingleCard.js

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -4,8 +4,8 @@ const SingleCard = ({ card, handleChoice, flipped, disabled }) => {
     return (
     <div className='card'>
         <div className={ flipped ? 'flipped' : '' }>
-            <img className="front" src={card.src} />
-            <img className="back" src="/img/cover.png" onClick={ !disabled ? () => handleChoice(card) : null } />
+            <img className="front" src={card.src} alt="image" />
+            <img className="back" src="/img/cover.png" onClick={ !disabled ? () => handleChoice(card) : null } alt="image" />
         </div>
     </div>
     );
